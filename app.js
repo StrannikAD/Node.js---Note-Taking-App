b02812160e1dfe22b23cd761b378c739962c5c53
Anna <a.dimitrova.au@gmail.com>
2017-05-28 11:47:59 +1000

    Refactored add functionality for reusability

--- app.js
+++ app.js
@@ -11,12 +11,20 @@ const yargs = require('yargs');
 const notes = require('./notes.js');
 
 const argv = yargs.argv;
-var command = process.argv[2];
+var command = argv._[0];
 console.log('Command: ', command);
 console.log('Yargs', argv);
 
 if (command === 'add') {
-  notes.addNote(argv.title, argv.body);
+  var note = notes.addNote(argv.title, argv.body);
+  if (note) {
+    console.log('Note created');
+    console.log('--');
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
+  } else {
+    console.log('Note title taken');
+  }
 } else if (command === 'list') {
   notes.getAll();
 } else if (command === 'read') {
@@ -24,5 +32,5 @@ if (command === 'add') {
 } else if (command === 'remove') {
   notes.removeNote(argv.title);
 } else {
-  console.log('Command not recognized.');
+  console.log('Command not recognized');
 }
--- notes.js
+++ notes.js
@@ -2,25 +2,31 @@ console.log('Starting notes.js');
 
 const fs = require('fs');
 
+var fetchNotes = () => {
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    return JSON.parse(notesString);
+  } catch (e) {
+    return [];
+  }
+};
+
+var saveNotes = (notes) => {
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
 var addNote = (title, body) => {
-  var notes = [];
+  var notes = fetchNotes();
   var note = {
     title,
     body
   };
-
-  try {
-    var notesString = fs.readFileSync('notes-data.json');
-    notes = JSON.parse(notesString);
-  } catch (e) {
-
-  }
-
   var duplicateNotes = notes.filter((note) => note.title === title);
 
   if (duplicateNotes.length === 0) {
-    notes.push(note); // .push adds the note object to the notes array
-    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    notes.push(note);
+    saveNotes(notes);
+    return note;
   }
 };
 
@@ -29,7 +35,7 @@ var getAll = () => {
 };
 
 var getNote = (title) => {
-  console.log('Getting note', title)
+  console.log('Getting note', title);
 };
 
 var removeNote = (title) => {
