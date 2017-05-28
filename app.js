59d9ad2699ab71f90610640e866b69ec97ac20c0
Anna <a.dimitrova.au@gmail.com>
2017-05-28 13:16:01 +1000

    Refactored app.js for reusability

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log('Starting app.js');
-
 // tells node that you want to
 // fetch all of the contents of
 // the fs module and store it
@@ -10,10 +8,31 @@ const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-const argv = yargs.argv;
+const titleOptions = {
+  describe: 'Title of note',
+  demand: true,
+  alias: 't'
+};
+const bodyOptions = {
+  describe: 'Body of note',
+  demand: true,
+  alias: 'b'
+};
+const argv = yargs
+  .command('add', 'Add a new note', {
+    title: titleOptions,
+    body: bodyOptions
+  })
+  .command('list', 'List all notes')
+  .command('read', 'Read a note', {
+    title: titleOptions,
+  })
+  .command('remove', 'Remove a note', {
+    title: titleOptions
+  })
+  .help()
+  .argv;
 var command = argv._[0];
-console.log('Command: ', command);
-console.log('Yargs', argv);
 
 if (command === 'add') {
   var note = notes.addNote(argv.title, argv.body);
